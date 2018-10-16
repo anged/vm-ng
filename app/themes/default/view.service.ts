32d3205cac651800abb1d0eb65e3e087778ec30f
Andrius Geduska <a.geduska@gmail.com>
2018-10-16 16:22:17 +0300

    quick UI and html fixes added

--- app/themes/default/view.service.ts
+++ app/themes/default/view.service.ts
@@ -19,46 +19,46 @@ export class ViewService {
   ) { }
 
   setmapElementRef(el: ElementRef): void {
-  	this.mapElementRef = el;
-	}
+    this.mapElementRef = el;
+  }
 
-	getMapElementRef(): ElementRef {
-	  return this.mapElementRef;
-	}
+  getMapElementRef(): ElementRef {
+    return this.mapElementRef;
+  }
 
-	// create themes' group layer and add all layers to it
-	createThemeLayers(snapshotUrl: string, queryParams) {
-	  const rasterLayers = this.mapService.getRasterLayers();
-	  //create theme main layers grouped
-	  //const themeGroupLayer = this.mapService.initGroupLayer("theme-group", "Main theme layers", "show");
+  // create themes' group layer and add all layers to it
+  createThemeLayers(snapshotUrl: string, queryParams) {
+    const rasterLayers = this.mapService.getRasterLayers();
+    //create theme main layers grouped
+    //const themeGroupLayer = this.mapService.initGroupLayer("theme-group", "Main theme layers", "show");
 
-	  //using lodash find and pick themeLayer from options
-	  let themeName = findKey(MapOptions.themes, { "id": snapshotUrl });
-	  let themeLayers = pick(MapOptions.themes, themeName)[themeName]["layers"];
+    //using lodash find and pick themeLayer from options
+    let themeName = findKey(MapOptions.themes, ['id', snapshotUrl]);
+    let themeLayers = pick(MapOptions.themes, themeName)[themeName]["layers"];
 
-	  forIn(themeLayers, (layer, key) => {
-	    this.mapService.pickMainThemeLayers(layer, key, queryParams);
-	  });
+    forIn(themeLayers, (layer, key) => {
+      this.mapService.pickMainThemeLayers(layer, key, queryParams);
+    });
 
-	  //set raster layers
-	  this.mapService.setRasterLayers(rasterLayers);
-	}
+    //set raster layers
+    this.mapService.setRasterLayers(rasterLayers);
+  }
 
-	//create sub Layers
-	//TODO remove sub layers and create custom widget with layers search
-	createSubLayers(queryParams, map = null) {
-	  // check if allLayers layer  exist
-	  const allLayersLayer = map.findLayerById('allLayers');
-	  if (typeof allLayersLayer === 'undefined') {
-	    // count sub layers and add to map if required
-	    const responseFeatures = this.mapService.fetchSublayersRequest(MapOptions.mapOptions.staticServices.commonMaps);
-	    this.mapService.addToMap(responseFeatures, queryParams);
-	    // TODO remove
-	    if (queryParams.allLayers && (queryParams.identify === "allLayers")) {
-	      //set sublayers state as we will load all layers layer on map
-	      this.menuService.setSubLayersState();
-	    };
-	  }
-	}
+  //create sub Layers
+  //TODO remove sub layers and create custom widget with layers search
+  createSubLayers(queryParams, map = null) {
+    // check if allLayers layer  exist
+    const allLayersLayer = map.findLayerById('allLayers');
+    if (typeof allLayersLayer === 'undefined') {
+      // count sub layers and add to map if required
+      const responseFeatures = this.mapService.fetchSublayersRequest(MapOptions.mapOptions.staticServices.commonMaps);
+      this.mapService.addToMap(responseFeatures, queryParams);
+      // TODO remove
+      if (queryParams.allLayers && (queryParams.identify === "allLayers")) {
+        //set sublayers state as we will load all layers layer on map
+        this.menuService.setSubLayersState();
+      };
+    }
+  }
 
 }
