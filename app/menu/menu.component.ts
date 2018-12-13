428da781a23872fe0af32f666c24dd18fa567fd9
Andrius Geduska <a.geduska@gmail.com>
2018-12-13 16:15:27 +0200

    showing layer tab when loading themes

--- app/menu/menu.component.ts
+++ app/menu/menu.component.ts
@@ -14,14 +14,14 @@ import watchUtils = require("esri/core/watchUtils");
 
 @Component({
   selector: 'menu-map',
-	entryComponents: [ProfileToolContainerComponent],
+  entryComponents: [ProfileToolContainerComponent],
   templateUrl: './app/menu/menu.component.html'//,
 })
 export class MenuComponent implements OnInit, OnDestroy {
   @Input() view: any;
   @Input() allLayerslayer: any;
   //@ViewChild(ProfileElevationComponent) profileElevationComponent: ProfileElevationComponent;
-	ProfileToolContainerComponent = ProfileToolContainerComponent;
+  ProfileToolContainerComponent = ProfileToolContainerComponent;
 
   mobileActive: boolean = false;
 
@@ -203,7 +203,20 @@ export class MenuComponent implements OnInit, OnDestroy {
           this.route = this.themeName;
         }
 
-        //console.log('Active Route', this.themeName, this.router.url, event);
+        // simply activate :target speudo class with location href
+        const hrefArray = window.location.href.split('#');
+
+        // do not activate :target class  when navigating to first page (lenght !==2)
+        //  or links doesn't have :target class
+        if (hrefArray.length === 2) {
+          const target = hrefArray.pop();
+          if (target === 'layers') {
+            window.location.href = window.location.href;
+          }
+
+        }
+
+        ///console.log('Active Route', this.themeName, this.router, this.router.url, event);
       });
   }
 
